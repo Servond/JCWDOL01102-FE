f3a47e08f7be2118a42b62739d6b2d298a467190
Galih Setyawan <galih.kamvax032@gmail.com>
2024-01-16 02:26:18 +0700

    Add authorization headers to address API calls

--- src/api/address.ts
+++ src/api/address.ts
@@ -1,16 +1,29 @@
 import { AddressAttributes } from "../data/address/interface";
+import { getToken } from "./admin/product";
 import { adminServer } from "./server";
 
 export const createAddress = (id: number, data: AddressAttributes) => {
-  return adminServer.post(`api/address/${id}`, data, {});
+  return adminServer.post(`api/address/${id}`, data, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
 };
 
 export const getAddressList = (id: number) => {
-  return adminServer.get(`api/address/${id}`, {});
+  return adminServer.get(`api/address/${id}`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
 };
 
 export const changeDefaultAddress = (id: number, addressId: number) => {
-  return adminServer.put(`api/address/${id}/${addressId}`, {});
+  return adminServer.put(`api/address/${id}/${addressId}`, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
 };
 
 export const updateAddress = (
@@ -18,5 +31,9 @@ export const updateAddress = (
   addressId: number,
   data: AddressAttributes
 ) => {
-  return adminServer.patch(`api/address/${id}/${addressId}`, data, {});
+  return adminServer.patch(`api/address/${id}/${addressId}`, data, {
+    headers: {
+      Authorization: getToken(),
+    },
+  });
 };
